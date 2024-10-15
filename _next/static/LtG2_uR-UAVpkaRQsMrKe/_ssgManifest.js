08d4fd66c42cdd97719ee6e9d7965e23f4c2ff47
amia westernvisayas <da6loms2021@gmail.com>
2024-10-15 16:14:44 +0800

    acap update
    acap update

---
+++ _next/static/LtG2_uR-UAVpkaRQsMrKe/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fadmin\u002Flogin","\u002Fbulletins","\u002Fbulletins\u002Fspecial-weather-forecast","\u002Fsuperadmin\u002Flogin","\u002Fbulletins\u002Fseasonal-outlook","\u002F","\u002Fcropping-calendar-v2","\u002Fagroclimatic-services","\u002Fweather-services","\u002Fbulletins\u002Fweather"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
